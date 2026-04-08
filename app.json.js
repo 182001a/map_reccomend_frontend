25c0170b4fc13647b43cecb733d1fc81f695d142
182001a <hisashi182001a@gmail.com>
2026-04-09 00:29:36 +0900

    初期設定完了

---
+++ app.json.js
@@ -0,0 +1,43 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "MapApp",
+    slug: "MapApp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAP_API_KEY
+      }
+    },
+    android: {
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAP_API_KEY
+        }
+      },
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    plugins: [
+      "expo-secure-store"
+    ]
+  }
+};
